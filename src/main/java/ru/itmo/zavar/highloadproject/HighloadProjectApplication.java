7b6aa87db75f0e5e2994ee73a4062906ea8f5ba7
Zavar30 <yarusking@mail.ru>
2023-10-11 17:58:16 +0300

    initial commit

---
+++ src/main/java/ru/itmo/zavar/highloadproject/HighloadProjectApplication.java
@@ -0,0 +1,13 @@
+package ru.itmo.zavar.highloadproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HighloadProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HighloadProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/itmo/zavar/highloadproject/HighloadProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.itmo.zavar.highloadproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+//@SpringBootTest
+class HighloadProjectApplicationTests {
+
+//    @Test
+    void contextLoads() {
+    }
+
+}
